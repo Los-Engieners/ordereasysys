5d03b49e00aeb826d3ff534c3a316726b3f63f97
Katherine Trigueros <triguerosk800@mail.com>
2024-08-13 13:49:33 -0600

    Prueba de error

--- src/main/java/org/ordereasy/controllers/OrdrController.java
+++ src/main/java/org/ordereasy/controllers/OrdrController.java
@@ -1,4 +1,9 @@
 package org.ordereasy.controllers;
 
+import org.ordereasy.services.interfaces.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class OrdrController {
+    @Autowired
+    private IRoleService roleService;
 }
